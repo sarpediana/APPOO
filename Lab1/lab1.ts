2d175bb5eb6b020d22cf09bdb3e73aff0f74654a
Diana <diana.sarpe96@gmail.com>
2018-02-26 23:35:00 +0200

    incapsulare + polimorfist+ abstract

--- Lab1/lab1.ts
+++ Lab1/lab1.ts
@@ -1,37 +1,65 @@
-class CardBank {
-    protected nrCard: number;
-    protected posesorCard: string;
-    protected balance: number;
+abstract class BankAccount {
+    private nr: string;
+    private posesorCard: string;
+    private balance: number;
+    private countTrasaction: number;
 
-    constructor(nrCard: number, posesorCard: string, balance: number) {
-        this.nrCard = nrCard;
+    constructor(nr: string, posesorCard: string, balance: number) {
+        this.nr = nr;
         this.balance = balance;
         this.posesorCard = posesorCard;
+        this.countTrasaction = 0;
     }
+
+    getBalance() {
+        return this.balance;
+    }
+    getCountTrasaction() {
+        return this.countTrasaction;
+    }
+    private setBalance(balance) {
+        this.balance = balance;
+    }
+    private setCountTrasaction() {
+        this.countTrasaction++;
+    }
+
+    Balance(balance: number) {
+        this.setBalance(balance);
+        this.setCountTrasaction();
+    }
+    abstract addMoney(quantity: number);
 }
 
-class Transaction extends CardBank {
-    protected countTrasaction: number;
-    protected sumTransaction: number;
+class Transaction extends BankAccount {
+    constructor(nr, posesorCard, balance) {
+        super(nr, posesorCard, balance);
+    }
+    addMoney(quantity) {
+        let balance = this.getBalance();
+        if (balance + quantity < 0) {
+            console.log('Bani insuficienti');
+        } else {
+            balance = balance + quantity;
+            this.Balance(balance);
+        }
+    }
 
-    constructor(nrCard, posesorCard, balance, sumTranstation: number) {
+}
+
+class Economy extends BankAccount {
+    constructor(nrCard, posesorCard, balance) {
         super(nrCard, posesorCard, balance);
-        this.sumTransaction = sumTranstation;
+    }
+    addMoney(interestRate) {
+        let balance = this.getBalance() + this.getBalance() * interestRate / 100;
+        this.Balance(balance);
     }
 }
 
-
-class Economy extends CardBank {
-    protected interestRate : number;
-
-    constructor(nrCard, posesorCard, balance, interestRate: number) {
-        super(nrCard, posesorCard, balance);
-        this.interestRate = interestRate;
-    }
-}
-
-let cardBank = new CardBank(2000355456, "SARPE DIANA", 2000);
-console.log(cardBank);
-
-let cardBank1 = new Transaction(20001313, "Diana Sarpe", 200, 12);
-console.log(cardBank1);
+let economy = new Economy('9999 9999 9999 9999', 'DIANA SARPE', 0);
+economy.addMoney(2.5);
+let transaction = new Transaction ('9 9 9 9 9 9 ', 'me me ', 0)
+transaction.addMoney(3546546);
+console.log(economy.getCountTrasaction(), economy);
+console.log(transaction.getCountTrasaction(), transaction);
